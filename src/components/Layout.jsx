653f005588eacabdcb80ce78fb5f393c396d1d1f
mgd0301 <108142769+mgd0301@users.noreply.github.com>
2025-06-24 08:33:13 -0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './header'
+import Header from './Header'
 import Footer from './Footer'
 import { FloatButton } from 'antd'
 import { WhatsAppOutlined } from '@ant-design/icons'
